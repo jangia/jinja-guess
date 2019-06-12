7b7e7e4ddc4e432d2263e235cf42aad81a086eab
Jan Giacomelli <jan.giacomelli@algoritmik.si>
2019-06-12 23:10:45 +0200

    Test

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 import os
 from sqla_wrapper import SQLAlchemy
 
-db = SQLAlchemy(os.getenv("DATABASE_URL", "sqlite:///localhost.sqlite?check_same_thread=false"))
+db = SQLAlchemy(os.getenv("DATABASE_URL", "postgres://svfcasbrkfdcow:600fc46d0c843ba4818b7a7f422d10785890950b82fd9c3d802eb89ce54b8fcd@ec2-79-125-4-72.eu-west-1.compute.amazonaws.com:5432/dfs70agbsg1d5n"))
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
